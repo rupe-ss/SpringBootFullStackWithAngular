4215a955f315701fb2a0d3a4aa5796c30b4987ac
rupesss-09 <rupesss09@gmail.com>
2021-06-10 02:19:08 -0500

    EmployeeRepo Interface is created and implementation of different methods in Service layer.

---
+++ src/main/java/springboot/angular/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package springboot.angular.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springboot.angular.domain.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
+}
---
+++ src/main/java/springboot/angular/service.java
@@ -0,0 +1,41 @@
+package springboot.angular;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import springboot.angular.domain.Employee;
+import springboot.angular.repo.EmployeeRepo;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class service {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public service(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employee;
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee employee(Long id){
+        return employeeRepo.findEmployeeById(id).
+                orElseThrow(() -> new UserNotFoundException("User by Id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
